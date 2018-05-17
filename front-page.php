d537e4905140774dd0b91e8761e43a5b7a6d2d12
Ilya Meerovich <ilyameerovich@Ilyas-MacBook-Air.local>
2018-05-16 19:53:12 -0700

    Added custom fields and basic styles for front page

--- front-page.php
+++ front-page.php
@@ -1,38 +1,25 @@
 <?php
 /**
- * The main template file.
+ * Template Name: Front Page
  *
  * @package RED_Starter_Theme
  */
 
 get_header(); ?>
+		<?php $logo = get_field('front_page_banner_logo'); ?>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if (have_posts()) : ?>
+			<header class="banner-section">
+				<div class="banner-section-img-wrapper">
+					<img src="<?php echo $logo ?>" alt="Pacwest Logo">
+				</div>
 
-			<?php if (is_home() && ! is_front_page()) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while (have_posts()) : the_post(); ?>
-
-				<?php get_template_part('template-parts/content'); ?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part('template-parts/content', 'none'); ?>
-
-		<?php endif; ?>
-
+				<h1><?php the_field('front_page_banner_text'); ?></h1>	
+			</header>
+	
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
---
+++ page-templates/front-page2.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Front Page2
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+
+
+
+<!-- THIS PAGE APPEARS TO BE REDUNDANT
+THIS PAGE APPEARS TO BE REDUNDANT
+THIS PAGE APPEARS TO BE REDUNDANT
+THIS PAGE APPEARS TO BE REDUNDANT
+THIS PAGE APPEARS TO BE REDUNDANT
+THIS PAGE APPEARS TO BE REDUNDANT
+THIS PAGE APPEARS TO BE REDUNDANT -->
+
+
+
+
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+			<header class="banner-section">
+				<h1><?php the_field('front_page_banner_text'); ?></h1>	
+			</header>
+	
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
