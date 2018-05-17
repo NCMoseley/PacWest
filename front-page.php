4989bb5c5419e95656c61398b9e95be478a04ea8
Ilya Meerovich <ilyameerovich@Ilyas-MacBook-Air.local>
2018-05-16 20:15:32 -0700

    Draft of intro section beneath banner

--- front-page.php
+++ front-page.php
@@ -7,6 +7,12 @@
 
 get_header(); ?>
 		<?php $logo = get_field('front_page_banner_logo'); ?>
+		<?php $banner_text = get_field('front_page_banner_text'); ?>
+
+		<?php $intro_text = get_field('front_page_intro_paragraph'); ?>
+		<?php $intro_icon = get_field('front_page_intro_icon'); ?>
+
+
 
 
 	<div id="primary" class="content-area">
@@ -17,8 +23,20 @@ get_header(); ?>
 					<img src="<?php echo $logo ?>" alt="Pacwest Logo">
 				</div>
 
-				<h1><?php the_field('front_page_banner_text'); ?></h1>	
+				<h1><?php echo $banner_text ?></h1>	
 			</header>
+
+
+			<section class="intro-section">
+				<div class="intro-section-paragraph">
+					<?php echo $intro_text ?>
+				</div>
+				<div class="intro-section-img-wrapper">
+					<img src="<?php echo $intro_icon ?>" alt="Leaf Icon">
+				</div>
+			</section>
+
+
 	
 		</main><!-- #main -->
 	</div><!-- #primary -->
