9ccfe321606f7b0d71bd76859341c5db4ec80059
Colin MJ <colinmj@telus.net>
2018-05-17 12:17:35 -0700

    Started about page

--- page-templates/about.php
+++ page-templates/about.php
@@ -7,8 +7,32 @@
 
 get_header(); ?>
 
+<?php $about_logo = get_field('about_banner_logo');?>
+<?php $about_header = get_field('about_header');?>
+<?php $about_company_header = get_field('about_company_header');?>
+<?php $about_company = get_field('about_company');?>
+<?php $about_header = get_field('about_header');?>
+<?php $about_header = get_field('about_header');?>
+
+
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<section class="about-wrapper">
+				<div class="about-menu"></div>
+
+				<div class="about-banner">
+
+					<div class="about-banner-content-wrapper">
+						<img src="<?php echo $about_logo ?>" alt="About page logo"/>
+						<h1> <?php echo $about_header ?></h1>
+					</div>
+					
+				</div>
+				<div class="about-our-company">
+
+				</div>
+				<div class="about-our-vision"></div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -16,8 +40,10 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
+
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
