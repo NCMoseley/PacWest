ddc2c5fbfc93003e9f2bd8e219228e98e11f7791
Ilya Meerovich <ilyameerovich@Ilyas-Air.vc.shawcable.net>
2018-05-18 10:59:20 -0700

    Added Press releases to news page - rough version

--- page-templates/news.php
+++ page-templates/news.php
@@ -6,13 +6,67 @@
  */
 
 get_header(); ?>
+  <?php $banner_text = get_field('news_page_banner_text'); ?>
+	<?php $logo = get_field('news_page_banner_logo');?>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-          <div>
-            <p>Hello from the news page!</p>
-          </div>
+      <header class="banner-section">
+				<div class="banner-section-img-wrapper">
+					<img src="<?php echo $logo ?>" alt="Pacwest Logo">
+				</div>
+
+				<h1>
+					<?php echo $banner_text ?>
+				</h1>
+      </header>
+
+
+      <section class="news-section">
+        <h2>Latest Press Releases</h2>
+
+        <div class="news-items-wrapper">
+          <?php $args = array( 'post_type' => 'post', 'posts_per_page' => -1 ); $query = new WP_Query($args);?>
+          <?php while ($query->have_posts()) : $query->the_post(); ?>
+          
+            <div class="news-item">
+              <div class="news-item-header">
+                <h3>
+                  <?php the_title(); ?>
+                </h3>
+                <span>
+                  <?php the_time(' F jS, Y') ?>
+                </span>
+              </div>
+
+              <div class="news-item-body">
+                <?php the_excerpt(); ?>
+              </div>
+            </div>
+
+
+            <div class="news-item-pdf">
+              <a href="<?php the_field('press_release_pdf')  ?>">PDF</a>
+            </div>
+
+
+            <p>
+              <a href="<?php the_permalink();?>"><i class="fa fa-chevron-right"></i></a>
+            </p>
+
+            
+          <?php endwhile; ?>
+          <?php wp_reset_query() ?>
+        </div>
+      </section>
+
+
+
+      
+
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
