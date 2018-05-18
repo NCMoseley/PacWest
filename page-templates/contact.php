4e6a83baa235efbc273c631994f765c13809acf0
Ilya Meerovich <ilyameerovich@Ilyas-MacBook-Air.local>
2018-05-17 19:48:53 -0700

    Draft of contact page

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -6,15 +6,35 @@
  */
 
 get_header(); ?>
+	<?php $banner_text = get_field('contact_page_banner_text'); ?>
+	<?php $main_text = get_field('contact_page_main_text'); ?>
+	<?php $logo = get_field('about_banner_logo');?>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+
+			<header class="banner-section">
+				<div class="banner-section-img-wrapper">
+					<img src="<?php echo $logo ?>" alt="Pacwest Logo">
+				</div>
+
+				<h1>
+					<?php echo $banner_text ?>
+				</h1>
+			</header>
+
+
+			<section class="contact-form">
+				<h2><?php echo $main_text ?></h2>
+			</section>
+
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
 			<?php endwhile; // End of the loop. ?>
+
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
