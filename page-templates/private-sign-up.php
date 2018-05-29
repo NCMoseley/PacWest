e8907fe0354e57489513ded79537c8a334a8cba4
NCMoseley <ncmoseley@gmail.com>
2018-05-29 16:36:05 -0700

    Add private page

---
+++ page-templates/private-sign-up.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Private Sign Up Page
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+	
+
+
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+      
+
+
+			<section class="main-section">
+			
+
+				<div class="private-container">
+
+				<img src="<?php echo get_template_directory_uri() . '/assets/Logos/PNG/PWC-Light.png'?>"/>
+
+				</div>
+
+
+
+			</section>
+
+
+
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_template_part('template-parts/sub-footer'); ?>
+<?php get_footer(); ?>
