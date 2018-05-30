8cbe397aa1f445be497e621da4e8c798a9d7feb9
NCMoseley <ncmoseley@gmail.com>
2018-05-29 18:01:00 -0700

    Add private page form

--- page-templates/private-sign-up.php
+++ page-templates/private-sign-up.php
@@ -14,20 +14,21 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-
-      
-
-
 			<section class="main-section">
-			
 
-				<div class="private-container">
+        	<div class="private-logo-container">
+            <img src="<?php echo get_template_directory_uri() . '/assets/Logos/PNG/PWC-Light.png'?>"/>
+		    	</div>
 
-				<img src="<?php echo get_template_directory_uri() . '/assets/Logos/PNG/PWC-Light.png'?>"/>
+				<div class="private-form-container">
 
-				</div>
+        	<?php while (have_posts()) : the_post(); ?>
 
+				    <?php get_template_part('template-parts/content', 'page'); ?>
 
+				  <?php endwhile; // End of the loop.?>
+
+        </div>
 
 			</section>
 
