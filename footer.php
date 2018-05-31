8a62ff101368cdb0abf1e697885f7ec12d0ae4a2
NCMoseley <ncmoseley@gmail.com>
2018-05-31 08:58:05 -0700

    Add links to media icons in the footer

--- footer.php
+++ footer.php
@@ -30,16 +30,16 @@
 					</div>
 					
 					<div class="footer-social-media">
-					<a href="https://www.facebook.com/">
+					<a href="https://www.facebook.com/pacwestcanopy">
 						<i class="fa fa-facebook" aria-hidden="true"></i>
 					</a>
-					<a href="https://twitter.com/">
+					<a href="https://twitter.com/pacwestcanopy">
 						<i class="fa fa-twitter" aria-hidden="true"></i>
 					</a>
-					<a href="https://www.linkedin.com/">	
+					<!-- <a href="https://www.linkedin.com/">	
 						<i class="fa fa-linkedin" aria-hidden="true"></i>
-					</a>
-					<a href="https://www.instagram.com/">	
+					</a> -->
+					<a href="https://www.instagram.com/pacwestcanopy/">	
 						<i class="fa fa-instagram" aria-hidden="true"></i>
 					</a>	
 					</div>
