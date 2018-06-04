787bcd849d0d3166aec33c9366a9b41ff95f48f5
Ilya Meerovich <ilyameerovich@Ilyas-MacBook-Air.local>
2018-06-03 20:08:09 -0700

    Merge Conflict

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function ($) {
       $(this)
         .parent()
         .next()
-        .slideToggle('swing');
+        .slideToggle(100);
       $(this)
         .children()
         .last()
@@ -202,48 +202,48 @@ jQuery(document).ready(function ($) {
 
   $('.logged-in .site-header').css('top', '32px');
 
-
+  var templateLocation = '';
 
   // // ADD MENU ITEM IN MOBILE
 
-  // function addMenuItem() {
-  //   var link = '';
-  //   if ($('body').hasClass('logged-in')) {
-  //     link = '/investor-portal';
-  //   } else {
-  //     link = '/investors'
-  //   }
+  function addMenuItem() {
+    var link = '';
+    if ($('body').hasClass('logged-in')) {
+      link = '/investor-portal';
+    } else {
+      link = '/investors'
+    }
 
-  //   $('.menu-item-163 .sub-menu').append(
-  //     '<li class="investors-link"><a href="' +
-  //     templateLocation.template_url + link +
-  //     '">Investors</a></li>'
-  //   );
+    $('.menu-item-163 .sub-menu').append(
+      '<li class="investors-link"><a href="' +
+      templateLocation.template_url + link +
+      '">Investors</a></li>'
+    );
 
-  //   investorVisbility();
-  //   $(window).resize(function () {
-  //     investorVisbility();
-  //   });
-  // }
-  // addMenuItem();
+    investorVisbility();
+    $(window).resize(function () {
+      investorVisbility();
+    });
+  }
+  addMenuItem();
 
-  // function investorVisbility() {
-  //   if ($(window).width() < 750) {
-  //     $('.investors-link').css('display', 'block');
-  //   } else {
-  //     $('.investors-link').css('display', 'none');
-  //   }
-  // }
+  function investorVisbility() {
+    if ($(window).width() < 750) {
+      $('.investors-link').css('display', 'block');
+    } else {
+      $('.investors-link').css('display', 'none');
+    }
+  }
 
 
-  // function investorRedirect() {
-  //   var url = templateLocation.template_url + '/investor-portal';
-  //   if ($('body').hasClass('logged-in')) {
-  //     $('.menu-item-163 > a').attr('href', url);
-  //     $('.sub-footer-investor-link').attr('href', url);
-  //   }
-  // }
-  // investorRedirect();
+  function investorRedirect() {
+    var url = templateLocation.template_url + '/investor-portal';
+    if ($('body').hasClass('logged-in')) {
+      $('.menu-item-163 > a').attr('href', url);
+      $('.sub-footer-investor-link').attr('href', url);
+    }
+  }
+  investorRedirect();
 
 
 }); 
