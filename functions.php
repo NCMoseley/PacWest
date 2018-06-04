1195370a98ab5aa793ebf99c3b9ec041057606be
Ilya Meerovich <ilyameerovich@Ilyas-MacBook-Air.local>
2018-06-03 19:16:33 -0700

    Merge Conflict

--- functions.php
+++ functions.php
@@ -122,7 +122,7 @@ function red_starter_scripts()
 
     wp_enqueue_script('red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true);
     wp_register_script('main', get_template_directory_uri() . '/build/js/main.min.js', array('jquery'), '1.0.0', true);
-        wp_localize_script( 'main', 'templateLocation', array( 'template_url' => get_bloginfo('url') ) ); 
+        // wp_localize_script( 'main', 'templateLocation', array( 'template_url' => get_bloginfo('url') ) ); 
         wp_enqueue_script('main');
 
 
--- js/main.js
+++ js/main.js
@@ -248,4 +248,4 @@ jQuery(document).ready(function ($) {
   investorRedirect();
 
 
-});
+}); 
