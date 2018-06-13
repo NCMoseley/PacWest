0114e7d5defb747a8649c6e47b8bc40e9b8e9eda
NCMoseley <ncmoseley@gmail.com>
2018-06-13 12:30:56 -0700

    Remove console . log

--- js/image.unveil.js
+++ js/image.unveil.js
@@ -3,9 +3,6 @@
 
   $(document).ready(function () {
 
-    // $('.banner-section-enhanced').css('display', 'block');
-    // $('.banner-section').hide();
-
     var menuItemName = document.querySelector('.current-menu-item');
 
     if (menuItemName !== null && menuItemName.textContent.includes('Investors')) {
@@ -17,13 +14,11 @@
     } else {
       currentTemplate = 'Home';
     }
-    console.log(currentTemplate);
     var hero = new Image();
     hero.src = '/pacwest/wp-content/themes/PacWest/assets/Stock_Photos/' + currentTemplate + '-optimized.jpg';
 
 
     $(hero).load(function () {
-      // $('.banner-section').addClass('banner-hero').fadeIn(2000);
       $('.banner-section').css('background-image', 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(' + $(this).attr('src') + ')').fadeIn(2000);
 
 
