560714a1935ffb44c8b5d4c11e64be19919deef6
NCMoseley <ncmoseley@gmail.com>
2018-06-15 13:34:48 -0700

    Add correct JS query for investor portal banner

--- js/image.unveil.js
+++ js/image.unveil.js
@@ -4,17 +4,19 @@
   $(document).ready(function () {
 
     var menuItemName = document.querySelector('.current-menu-item');
-
     if (menuItemName !== null && menuItemName.textContent.includes('Investors')) {
       var currentTemplate = 'Investors';
     } else if (menuItemName !== null && menuItemName.textContent.includes('Corporate Governance')) {
       currentTemplate = 'Team';
+    } else if (menuItemName !== null && menuItemName.textContent.includes('Investor Portal')) {
+      currentTemplate = 'Investors';
     } else if (menuItemName !== null) {
       currentTemplate = menuItemName.textContent;
     } else {
       currentTemplate = 'Home';
     }
     var hero = new Image();
+    // note: /wp-content/themes/PacWest/assets/Stock_Photos/ is the link for the live site
     hero.src = '/pacwest/wp-content/themes/PacWest/assets/Stock_Photos/' + currentTemplate + '-optimized.jpg';
 
 
