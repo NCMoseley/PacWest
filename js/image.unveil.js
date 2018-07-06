ba60e37cbaec7c0f328987b0b9ccfa40a75b41dd
NCMoseley <ncmoseley@gmail.com>
2018-07-05 17:01:59 -0700

    Add changes to product page

--- js/image.unveil.js
+++ js/image.unveil.js
@@ -1,14 +1,25 @@
-(function ($) {
-
-
-  $(document).ready(function () {
-
+(function($) {
+  $(document).ready(function() {
     var menuItemName = document.querySelector('.current-menu-item');
-    if (menuItemName !== null && menuItemName.textContent.includes('Investors')) {
+    if (
+      menuItemName !== null &&
+      menuItemName.textContent.includes('Investors')
+    ) {
       var currentTemplate = 'Investors';
-    } else if (menuItemName !== null && menuItemName.textContent.includes('Corporate Governance')) {
+    } else if (
+      menuItemName !== null &&
+      menuItemName.textContent.includes('Corporate Governance')
+    ) {
       currentTemplate = 'Team';
-    } else if (menuItemName !== null && menuItemName.textContent.includes('Investor Portal')) {
+    } else if (
+      menuItemName !== null &&
+      menuItemName.textContent.includes('Products')
+    ) {
+      currentTemplate = 'Products';
+    } else if (
+      menuItemName !== null &&
+      menuItemName.textContent.includes('Investor Portal')
+    ) {
       currentTemplate = 'Investors';
     } else if (menuItemName !== null) {
       currentTemplate = menuItemName.textContent;
@@ -17,16 +28,20 @@
     }
     var hero = new Image();
     // note: /wp-content/themes/PacWest/assets/Stock_Photos/ is the link for the live site
-    hero.src = '/pacwest/wp-content/themes/PacWest/assets/Stock_Photos/' + currentTemplate + '-optimized.jpg';
+    hero.src =
+      '/pacwest/wp-content/themes/PacWest/assets/Stock_Photos/' +
+      currentTemplate +
+      '-optimized.jpg';
 
-
-    $(hero).load(function () {
-      $('.banner-section').css('background-image', 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(' + $(this).attr('src') + ')').fadeIn(2000);
-
-
+    $(hero).load(function() {
+      $('.banner-section')
+        .css(
+          'background-image',
+          'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(' +
+            $(this).attr('src') +
+            ')'
+        )
+        .fadeIn(2000);
     });
-
   });
-
-
 })(window.jQuery);
